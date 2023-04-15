ccc98a80992fe8cb431a9036de9cbadd669f72b9
Mehmet-Durna <r0900513@student.thomasmore.be>
2023-04-15 02:02:52 +0200

    adding document is added in register function

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:brew/models/model_user.dart';
+import 'package:brew/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
@@ -47,6 +48,12 @@ class AuthService {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       User? user = result.user;
+
+      //create a new document for the user with the uid
+
+      await DatabaseService(uid: user!.uid)
+          .updateUserData('0', 'new crew member', 100);
+
       return _userFromFirebaseUser(user!);
     } catch (e) {
       print(e.toString());
